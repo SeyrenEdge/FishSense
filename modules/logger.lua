dd5b481976b3b1f6ff8002de2414e8af96764d6a
SeyrenEdge <108256508+SeyrenEdge@users.noreply.github.com>
2026-07-22 11:27:32 +0200

    Update logger.lua

--- modules/logger.lua
+++ modules/logger.lua
@@ -1,30 +1,15 @@
 local logger = {}
 
-local PREFIX = '[FishSense] '
-
-local function printMessage(color, message)
-
-    print(chat.header(PREFIX) ..
-          chat.color1(color, message))
-
-end
-
 function logger.info(message)
-
-    printMessage(2, message)
-
+    print('[FishSense] ' .. message)
 end
 
 function logger.warning(message)
-
-    printMessage(57, message)
-
+    print('[FishSense] Warning: ' .. message)
 end
 
 function logger.error(message)
-
-    printMessage(167, message)
-
+    print('[FishSense] Error: ' .. message)
 end
 
 return logger
