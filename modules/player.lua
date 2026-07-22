0ca9dee2125a93a0cebba19508b5ce9b1ef38f68
SeyrenEdge <108256508+SeyrenEdge@users.noreply.github.com>
2026-07-22 12:51:07 +0200

    Enhance player module with job names and updates
    Added job names mapping and updated player data retrieval.

--- modules/player.lua
+++ modules/player.lua
@@ -9,15 +9,63 @@ local data = {
     zone = '',
 }
 
+local jobNames = {
+    [1] = 'WAR',
+    [2] = 'MNK',
+    [3] = 'WHM',
+    [4] = 'BLM',
+    [5] = 'RDM',
+    [6] = 'THF',
+    [7] = 'PLD',
+    [8] = 'DRK',
+    [9] = 'BST',
+    [10] = 'BRD',
+    [11] = 'RNG',
+    [12] = 'SAM',
+    [13] = 'NIN',
+    [14] = 'DRG',
+    [15] = 'SMN',
+    [16] = 'BLU',
+    [17] = 'COR',
+    [18] = 'PUP',
+    [19] = 'DNC',
+    [20] = 'SCH',
+    [21] = 'GEO',
+    [22] = 'RUN',
+}
+
 function player.initialize()
-    -- Ashita integration will be added in the next feature.
+    player.update()
 end
 
 function player.update()
-    -- Refresh player data.
+
+    local mem = AshitaCore:GetMemoryManager()
+    if mem == nil then
+        return
+    end
+
+    local p = mem:GetPlayer()
+    if p == nil then
+        return
+    end
+
+    data.name = p:GetName() or ''
+
+    local mainId = p:GetMainJob()
+    local subId  = p:GetSubJob()
+
+    data.mainJob = jobNames[mainId] or tostring(mainId)
+    data.subJob  = jobNames[subId] or tostring(subId)
+
+    data.mainLevel = p:GetMainJobLevel()
+    data.subLevel  = p:GetSubJobLevel()
+
+    -- Zone will be implemented in the next step.
 end
 
 function player.getInfo()
+    player.update()
     return data
 end
 
